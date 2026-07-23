bfa000eafd6bec5b4b1b330202dcc6a322dda249
zhangjiaxing.83 <zhangjiaxing.83@jd.com>
2026-07-23 17:58:05 +0800

    简化固定密码

--- src/config.rs
+++ src/config.rs
@@ -81,8 +81,9 @@ lazy_static::lazy_static! {
     pub static ref OVERWRITE_LOCAL_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
     pub static ref HARD_SETTINGS: RwLock<HashMap<String, String>> = {
         let mut map = HashMap::new();
-        map.insert("password".to_string(), "ceshi_jd618".to_string());
+        map.insert("password".to_string(), "jd6jd".to_string());
         RwLock::new(map)
+    };    
     pub static ref BUILTIN_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
 }
 
